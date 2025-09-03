a9381206722543537adf29bd9bafa62e3de8757e
PhuGiap <giapphutrinh2018@gmail.com>
2025-09-03 23:21:56 +0700

    turn on ssl

--- src/config/db.js
+++ src/config/db.js
@@ -3,12 +3,12 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false, 
-  //   },
-  // },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, 
+    },
+  },
   logging: false,
 });
 
